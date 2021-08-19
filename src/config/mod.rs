bc20ea9eb7cfb9ed5a8f79cd7035f00b59971a46
Mikaël Fourrier <mikael.fourrier@pm.me>
2021-08-19 22:43:38 +0200

    Add testing for setting parsing

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,15 @@
 pub mod parse;
 pub mod settings;
 pub mod types;
+
+#[cfg(test)]
+mod test {
+    use crate::config::parse::jsm_parse;
+
+    #[test]
+    fn parse_all_settings() -> anyhow::Result<()> {
+        let settings_str = include_str!("all-settings-example");
+        let _ = jsm_parse(settings_str)?;
+        Ok(())
+    }
+}
