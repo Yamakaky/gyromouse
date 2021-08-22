aec9dd4171010ec9f51281089aff67c33329fc09
Mikaël Fourrier <mikael.fourrier@pm.me>
2021-08-22 14:16:21 +0200

    Improve help message

--- src/opts.rs
+++ src/opts.rs
@@ -2,17 +2,25 @@ use std::{path::PathBuf, str::FromStr};
 
 use clap::Clap;
 
+/// Input mapper from gamepad keypress and movement to mouse and keyboard.
+///
+/// See <https://github.com/Yamakaky/gyromouse/blob/master/README.md> for more
+/// information about features and configuration file format.
 #[derive(Debug, Clap)]
 #[clap(version = clap::crate_version!())]
 #[clap(setting = clap::AppSettings::ColoredHelp)]
 pub struct Opts {
+    /// Force the use of a specific backend for gamepad access.
     #[clap(short, long)]
+    #[clap(setting = clap::ArgSettings::Hidden)]
+    #[clap(possible_values = &["sdl2", "hidapi"])]
     pub backend: Option<Backend>,
     #[clap(subcommand)]
     pub cmd: Option<Cmd>,
 }
 
 #[derive(Debug, Clap)]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
 pub enum Backend {
     #[cfg(feature = "sdl2")]
     Sdl,
@@ -21,15 +29,23 @@ pub enum Backend {
 }
 
 #[derive(Debug, Clap)]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
 pub enum Cmd {
+    /// Validate the syntax of a configuration file.
     Validate(Run),
+    /// Compute the value of REAL_WORLD_CALIBRATION.
+    #[clap(setting = clap::AppSettings::Hidden)]
     FlickCalibrate,
+    /// Run the program using the specified configuration file.
     Run(Run),
+    /// List connected gamepads.
     List,
 }
 
 #[derive(Debug, Clap)]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
 pub struct Run {
+    /// Configuration file to use.
     pub mapping_file: PathBuf,
 }
 
